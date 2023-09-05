d52b6c95cb75f85881f794313015dfc3ca3623cd
vikramprabhur <125321436+vikramprabhur@users.noreply.github.com>
2023-09-05 11:37:02 -0700

    Add files via upload

---
+++ pacmen.js
@@ -0,0 +1,90 @@
+
+    var pos = 0;
+    
+    var focus = 0;
+    
+    var direction = 0;
+
+    const pacArray = [
+      ["./images/PacMan1.png", "./images/PacMan2.png"],
+      ["./images/PacMan3.png", "./images/PacMan4.png"],
+    ];
+    
+    const pacMen = [];
+
+
+    function setToRandom(scale) {
+      return {
+        x: Math.random() * scale,
+        y: Math.random() * scale,
+      };
+    }
+
+
+    function makePac() {
+      
+      let velocity = setToRandom(10);
+      let position = setToRandom(200);
+
+      let game = document.getElementById("game");
+      let newimg = document.createElement("img");
+      
+      newimg.style.position = "absolute";
+      newimg.src = "./images/PacMan2.png";
+      newimg.width = 100;
+      newimg.style.left = position.x;
+      newimg.style.top = position.y;
+      
+      game.appendChild(newimg);
+
+      return {
+        position,
+        velocity,
+        newimg
+      };
+    }
+
+
+    function update() {
+      
+      pacMen.forEach((item) => {
+        
+        checkCollisions(item);
+
+        item.position.x += item.velocity.x;
+        item.position.y += item.velocity.y;
+        
+        focus = 1 - focus;
+        direction = item.velocity.x > 0 ? 0 : 1; 
+        item.newimg.src = pacArray [direction][focus];
+
+        item.newimg.style.left = item.position.x;
+        item.newimg.style.top = item.position.y;
+      });
+      setTimeout(update, 80);
+    }
+
+
+    function checkCollisions(item) {
+      if (
+        item.position.x + item.velocity.x + item.newimg.width >
+          window.innerWidth ||
+        item.position.x + item.velocity.x < 0
+      )
+        item.velocity.x = -item.velocity.x;
+
+      if (
+        item.position.y + item.velocity.y + item.newimg.height >
+          window.innerHeight ||
+        item.position.y + item.velocity.y < 0
+      )
+        item.velocity.y = -item.velocity.y;
+    }
+
+
+    function makeOne() {
+      pacMen.push(makePac());
+      if(pacMen.length % 2 === 0){
+        pacMen.push(makePac());
+      }
+    }
